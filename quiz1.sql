35ce238007de13862db2571e48e2c624a9f7fc44
samgbrown16-crypto <samgbrown16@gmail.com>
2026-01-08 11:08:30 +0200

    Add files via upload

---
+++ quiz1.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 08, 2026 at 10:06 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quiz1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branch`
+--
+
+CREATE TABLE `branch` (
+  `branch_id` int(11) NOT NULL,
+  `branch_name` varchar(50) DEFAULT NULL,
+  `mgr_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `branch`
+--
+
+INSERT INTO `branch` (`branch_id`, `branch_name`, `mgr_id`) VALUES
+(1, 'Corporate', 108),
+(2, 'Scranton', 101),
+(3, 'Stamford', 102);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `client_id` int(11) NOT NULL,
+  `client_name` varchar(20) DEFAULT NULL,
+  `branch_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`client_id`, `client_name`, `branch_id`) VALUES
+(400, 'Dunmore Highschool', 2),
+(401, 'Lackawana Country', 2),
+(402, 'FedEx', 3),
+(403, 'John Law LLC', 3),
+(404, 'Scranton Whitepages', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `emp_id` int(11) NOT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `sex` char(1) DEFAULT NULL,
+  `salary` decimal(10,2) DEFAULT NULL,
+  `branch_id` int(11) DEFAULT NULL,
+  `super_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`emp_id`, `first_name`, `last_name`, `birth_date`, `sex`, `salary`, `branch_id`, `super_id`) VALUES
+(100, 'Jan', 'Levinson', '1961-05-11', 'F', 110000.00, 1, NULL),
+(101, 'Michael', 'Scott', '1964-03-15', 'M', 75000.00, 2, 100),
+(102, 'Josh', 'Porter', '1969-09-05', 'M', 78000.00, 3, 100),
+(103, 'Angela', 'Martin', '1971-06-25', 'F', 63000.00, 2, 101),
+(104, 'Andy', 'Bernard', '1973-07-22', 'M', 65000.00, 3, 101);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `works_with`
+--
+
+CREATE TABLE `works_with` (
+  `emp_id` int(11) NOT NULL,
+  `client_id` int(11) DEFAULT NULL,
+  `total_sales` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `works_with`
+--
+
+INSERT INTO `works_with` (`emp_id`, `client_id`, `total_sales`) VALUES
+(101, 401, 267000.00),
+(104, 403, 5000.00),
+(105, 402, 22500.00),
+(107, 400, 55000.00);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `branch`
+--
+ALTER TABLE `branch`
+  ADD PRIMARY KEY (`branch_id`);
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`client_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`emp_id`);
+
+--
+-- Indexes for table `works_with`
+--
+ALTER TABLE `works_with`
+  ADD PRIMARY KEY (`emp_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
